10bcbad43fdb486e4e853d6ad1647f299a5b70a7
YuriyRy <80952028+YuriyRy@users.noreply.github.com>
2021-04-19 16:33:11 +0300

    Add files via upload

--- practice.test.js
+++ practice.test.js
@@ -1,4 +1,8 @@
-var {getDays, isOdd, every} = require('./practice.js')
+var {
+    getDays,
+    isOdd,
+    every
+} = require('./practice.js')
 
 describe('getDays', function () {
     it('should be defined', function () {
@@ -40,18 +44,50 @@ describe('getDays', function () {
 describe('isOdd', function () {
     num = new Number()
     it("should be defined", function () {
-      expect(Number.prototype.isOdd).toBeDefined();
-       });
-        it("should be a function", function () {
+        expect(Number.prototype.isOdd).toBeDefined();
+    });
+    it("should be a function", function () {
         expect(typeof Number.prototype.isOdd).toBe("function");
-      });
-        it("must be false", function () {
+    });
+    it("must be false", function () {
         expect(num.isOdd(22)).toBe(false);
-      });
-        it("must be true", function () {
-          expect(num.isOdd(3)).toBe(true);
-      });
-      it("shoudn't work if arg is not number", function () {
+    });
+    it("must be true", function () {
+        expect(num.isOdd(3)).toBe(true);
+    });
+    it("shoudn't work if arg is not number", function () {
         expect(num.isOdd('fffffff')).toBe('error');
     });
-  });
+});
+
+//
+describe('every', function () {
+    it('should be defined', function () {
+        expect(every).toBeDefined()
+    })
+    it('should be a function', function () {
+        expect(typeof every).toBe('function')
+    })
+    it("shouldn't work only with two arguments", function () {
+        expect(every()).toBe('error')
+    })
+    it("shouldn't work without one arguments", function () {
+        expect(every([222, 2])).toBe('error')
+    })
+    it("shouldn't work if first arg is not array", function () {
+        expect(every('sfsgfg', () => {})).toBe('error')
+    })
+    it("shouldn't work if second arg is not a function", function () {
+        expect(every([9, 4, 7, 4], 'agaf')).toBe('error')
+    })
+    it('should work correct with 2 arguments', function () {
+        expect(every([8, 2, 4], function (num) {
+            return num % 2 === 0
+        })).toBe(true)
+    })
+    it('should work correct with 2 arguments', function () {
+        expect(every([8, 7, 4], function (num) {
+            return num % 2 === 0
+        })).toBe(false)
+    })
+})
